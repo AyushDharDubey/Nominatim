dbe419518be510b97172c040a62430ff02e092e6
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-12-11 21:47:21 +0530

    optimized search with predefined idx_search_name_name_vector GIN index

--- lib-sql/functions/tiger_lookup.sql
+++ lib-sql/functions/tiger_lookup.sql
@@ -16,14 +16,35 @@ CREATE OR REPLACE FUNCTION lookup_road_in_search_name(
   AS $$
 DECLARE
   out_place_id BIGINT;
+  search_tokens INTEGER[];
 
 BEGIN
+  -- extract tokens
+  in_token_info := COALESCE(in_token_info, '[]'::jsonb);
+
+-- Convert JSONB array to PostgreSQL Integer Array
+  SELECT ARRAY(
+      SELECT jsonb_array_elements_text(
+          CASE WHEN jsonb_typeof(in_token_info) = 'array' THEN in_token_info
+          ELSE '[]'::jsonb END -- if NULL
+      )::int
+  ) INTO search_tokens;
+
+  IF array_length(search_tokens, 1) IS NULL THEN
+    RETURN NULL;
+  END IF;
+
+
   SELECT place_id INTO out_place_id 
-      FROM search_name
-    WHERE token_matches_street(in_token_info, name_vector)
-      AND centroid && ST_Expand(in_centroid, 0.015)
-      AND address_rank between 26 and 27
-    ORDER BY ST_Distance(centroid, in_centroid) ASC limit 1;
+    FROM search_name
+    WHERE 
+        -- finds rows where name_vector shares elements with search tokens.
+        name_vector && search_tokens
+        -- limits search area
+        AND centroid && ST_Expand(in_centroid, 0.015) 
+        AND address_rank BETWEEN 26 AND 27
+    ORDER BY ST_Distance(centroid, in_centroid) ASC 
+    LIMIT 1;
 
   RETURN out_place_id;
 END;
